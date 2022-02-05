86e40e94824ea3273d5fa307d55c451a96ce61ad
desinobita <84653553+desinobita@users.noreply.github.com>
2022-02-05 20:23:41 +0530

    Update antiflood.py

--- MarinRobot/modules/antiflood.py
+++ MarinRobot/modules/antiflood.py
@@ -382,6 +382,21 @@ def __chat_settings__(chat_id, user_id):
 
 __mod_name__ = "Anti-Flood"
 
+__help__ = Hᴇʀᴇ Iꜱ Tʜᴇ Hᴇʟᴘ「「Anti-Spam」」 Mᴏᴅᴜʟᴇ:
+
+Admin Commands only:
+
+ /antispam - <on/off/yes/no> Will toggle our antispam tech or return your current settings.
+
+Anti-Spam used by bot devs to ban spammers across all groups. This helps protect
+you and your groups by removing spam flooders as quickly as possible.
+Note: Users can appeal gbans or report spammers at @SuzuneSupport 
+
+This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
+*What is SpamWatch?*
+SpamWatch maintains a large constantly updated ban-list of spambots trolls bitcoin spammers and unsavoury characters
+Constantly help banning spammers off from your group automatically So you wont have to worry about spammers storming your group.
+*Note:* Users can appeal spamwatch bans at @MarinXSupport 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.chat_type.groups,
     check_flood,
